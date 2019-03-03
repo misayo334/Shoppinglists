5cce7a09c8e4adf1a365999bc38c68ad67547dae
misayo <misayo334@hotmail.com>
2019-03-03 03:26:21 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="center jumbotron">
+    <div class="jumbotron jumbotron-fluid">
         <div class="text-center">
-            <p class="btn btn-outline-success btn-sm">Login button here</p>
-            <p class="btn btn-outline-success btn-sm">Register button here</p>
+            <h1>Welcome!</h1>
+            <br>
+            <hr class="my-4">
+            
+            <div>
+                <p class="btn btn-outline-success btn-sm">Login button here</p>
+                
+                
+                {!! link_to_route('signup.get', 'Register now!', [], ['class' => 'btn btn-outline-success btn-lg']) !!}
+            </div>
         </div>
     </div>
+
 @endsection
